41cfc0beda7df5d0a3ce740480dec736b1e86ed5
jayapal <allwinandrews@gmail.com>
2021-11-23 19:11:17 +0530

    Adds LoadingSpinner

---
+++ src/components/Layout/LoadingSpinner.js
@@ -0,0 +1,3 @@
+export default function LoadingSpinner() {
+  return <span className="spinner-border spinner-border-sm"></span>;
+}
--- src/components/UserDetailComponent/index.js
+++ src/components/UserDetailComponent/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { useState } from "react";
+import LoadingSpinner from "../Layout/LoadingSpinner";
 
 export default function UserDetailComponent({
   avatar,
@@ -7,10 +8,15 @@ export default function UserDetailComponent({
   id,
   email,
 }) {
+  const [isImageLoad, setIsImageLoad] = useState(true);
+
+  const handleImageLoad = () => setIsImageLoad(false);
+
   return (
     <>
       <header className="jumbotron">
-        <img src={avatar} alt="avatar" />
+        {isImageLoad && <LoadingSpinner />}
+        <img src={avatar} alt="avatar" onLoad={handleImageLoad} />
         <h3>
           <strong>{first_name + " " + last_name}</strong>
         </h3>
